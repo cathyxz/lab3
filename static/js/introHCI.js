e436dd32b53f9bcaaeabcd2b54860c1ecd441f25
Cathy Zhu <cathyzhu@stanford.edu>
2014-01-24 19:57:59 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,47 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Hello World!");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	
+	$("#submitBtn").click(updateProject); 
 }
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project");
+ 	var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    console.log(description);
+    if (description.is(":visible")) { 
+       $(description).hide();
+    } else { 
+		$(description).show();    
+	}
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
+
+
+
+
